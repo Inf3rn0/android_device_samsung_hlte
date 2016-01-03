1e084d23b74f1214093c7446e1f1550141358412
Morningstar <admirethedistance@gmail.com>
2016-01-02 17:12:09 -0700

    bring remote up to date with local
    Change-Id: I3b5a9b6e22259bf8b79ec7659b1ec4e28107c1aa

--- init/init_hlte.cpp
+++ init/init_hlte.cpp
@@ -60,24 +60,116 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     property_get("ro.bootloader", bootloader);
 
-    if (strstr(bootloader, "N900S")) {
-        /* hlteskt */
-        property_set("ro.build.fingerprint", "samsung/hlteskt/hlte:4.4.2/KOT49H/N900SKSUFNH4:user/release-keys");
-        property_set("ro.build.description", "hlteskt-user 4.4.2 KOT49H N900SKSUFNH4 release-keys");
-        property_set("ro.product.model", "SM-N900S");
-        property_set("ro.product.device", "hlteskt");
+    if (strstr(bootloader, "N900A")) {
+        /* hlteatt */
+        property_set("ro.build.fingerprint", "samsung/hlteuc/hlteatt:5.0/LRX21V/N900AUCUEOC1:user/morningstar");
+        property_set("ro.build.description", "hlteuc-user 5.0 LRX21V N900AUCUEOC1 morningstar");
+        property_set("ro.product.model", "SM-N900A");
+        property_set("ro.product.name", "hlteuc");
+        property_set("ro.product.device", "hlteatt");
+    } else if (strstr(bootloader, "N900W8")) {
+        /* hltecan */
+        if (strstr(bootloader, "N900W8UB")) {
+            /* hltecan/ub */
+            property_set("ro.build.fingerprint", "samsung/hlteub/hltecan:5.0/LRX21V/N900W8UBU2DOI2:user/morningstar");
+            property_set("ro.build.description", "hlteub-user 5.0 LRX21V N900W8UBU2DOI2 morningstar");
+            property_set("ro.product.name", "hlteub");
+        } else {
+            /* hltecan/vl */
+            property_set("ro.build.fingerprint", "samsung/hltevl/hltecan:5.0/LRX21V/N900W8VLU2DOH1:user/morningstar");
+            property_set("ro.build.description", "hltevl-user 5.0 LRX21V N900W8VLU2DOH1 morningstar");
+            property_set("ro.product.name", "hltevl");
+        }
+        property_set("ro.product.model", "SM-N900W8");
+        property_set("ro.product.device", "hltecan");
+    } else if (strstr(bootloader, "SC01F")) {
+        /* hltedcm */
+        property_set("ro.build.fingerprint", "samsung/SC-01F/SC-01F:5.0/LRX21V/SC01FOMUGOI4:user/morningstar");
+        property_set("ro.build.description", "hltedcm-user 5.0 LRX21V SC01FOMUGOI4 morningstar");
+        property_set("ro.product.model", "SC-01F");
+        property_set("ro.product.name", "hltedcm");
+        property_set("ro.product.device", "hltedcm");
+    } else if (strstr(bootloader, "SCL22")) {
+        /* hltekdi */
+        property_set("ro.build.fingerprint", "KDDI/SCL22/SCL22:5.0/LRX21V/SCL22KDU2GOH7:user/morningstar");
+        property_set("ro.build.description", "hltekdi-user 5.0 LRX21V SCL22KDU2GOH7 morningstar");
+        property_set("ro.product.model", "SCL22");
+        property_set("ro.product.name", "hltekdi");
+        property_set("ro.product.device", "hltekdi");
     } else if (strstr(bootloader, "N900K")) {
         /* hltektt */
-        property_set("ro.build.fingerprint", "samsung/hltektt/hlte:4.4.2/KOT49H/N900KKKUFNI1:user/release-keys");
-        property_set("ro.build.description", "hltektt-user 4.4.2 KOT49H N900KKKUFNI1 release-keys");
+        property_set("ro.build.fingerprint", "samsung/hltektt/hltektt:5.0/LRX21V/N900KKKU0GOF2:user/morningstar");
+        property_set("ro.build.description", "hltektt-user 5.0 LRX21V N900KKKU0GOF2 morningstar");
         property_set("ro.product.model", "SM-N900K");
+        property_set("ro.product.name", "hltektt");
         property_set("ro.product.device", "hltektt");
+        } else if (strstr(bootloader, "N900L")) {
+        /* hltelgt */
+        property_set("ro.build.fingerprint", "samsung/hltelgt/hltelgt:5.0/LRX21V/N900LKLU0GOJ4:user/morningstar");
+        property_set("ro.build.description", "hltelgt-user 5.0 LRX21V N900LKLU0GOJ4 morningstar");
+        property_set("ro.product.model", "SM-N900L");
+        property_set("ro.product.name", "hltelgt");
+        property_set("ro.product.device", "hltelgt");
+    } else if (strstr(bootloader, "N900S")) {
+        /* hlteskt */
+        property_set("ro.build.fingerprint", "samsung/hlteskt/hlteskt:5.0/LRX21V/N900SKSU0GOJ2:user/morningstar");
+        property_set("ro.build.description", "hlteskt-user 5.0 LRX21V N900SKSU0GOJ2 morningstar");
+        property_set("ro.product.model", "SM-N900S");
+        property_set("ro.product.name", "hlteskt");
+        property_set("ro.product.device", "hlteskt");
+    } else if (strstr(bootloader, "N900P")) {
+        /* hltespr */
+        property_set("ro.build.fingerprint", "samsung/hltespr/hltespr:5.0/LRX21V/N900PVPUEOH1:user/morningstar");
+        property_set("ro.build.description", "hltespr-user 5.0 LRX21V N900PVPUEOH1 morningstar");
+        property_set("ro.product.model", "SM-N900V");
+        property_set("ro.product.name", "hltespr");
+        property_set("ro.product.device", "hltespr");
+    } else if (strstr(bootloader, "N900T")) {
+        /* hltetmo */
+        property_set("ro.build.fingerprint", "samsung/hltetmo/hltetmo:5.0/LRX21V/N900TUVUFOB6:user/morningstar");
+        property_set("ro.build.description", "hltetmo-user 5.0 LRX21V N900TUVUFOB6 morningstar");
+        property_set("ro.product.model", "SM-N900T");
+        property_set("ro.product.name", "hltetmo");
+        property_set("ro.product.device", "hltetmo");
+    } else if (strstr(bootloader, "N900R4")) {
+        /* hlteusc */
+        property_set("ro.build.fingerprint", "samsung/hlteusc/hlteusc:5.0/LRX21V/N900R4TYUDOH1:user/morningstar");
+        property_set("ro.build.description", "hlteusc-user 5.0 LRX21V N900R4TYUDOH1 morningstar");
+        property_set("ro.product.model", "SM-N900R4");
+        property_set("ro.product.name", "hlteusc");
+        property_set("ro.product.device", "hlteusc");
+    } else if (strstr(bootloader, "N900V")) {
+        /* hltevzw */
+        property_set("ro.build.fingerprint", "Verizon/hltevzw/hltevzw:5.0/LRX21V/N900VVRUEOF1:user/morningstar");
+        property_set("ro.build.description", "hltevzw-user 5.0 LRX21V N900VVRUEOF1 morningstar");
+        property_set("ro.product.model", "SM-N900V");
+        property_set("ro.product.name", "hltevzw");
+        property_set("ro.product.device", "hltevzw");
     } else {
-        /* hltexx */
-        property_set("ro.build.fingerprint", "samsung/hltexx/hlte:4.4.2/KOT49H/N9005XXUENC2:user/release-keys");
-        property_set("ro.build.description", "hltexx-user 4.4.2 KOT49H N9005XXUENC2 release-keys");
+        /* hlte */
+        if (strstr(bootloader, "N9005DX")) {
+            /* hlte/dx */
+            property_set("ro.build.fingerprint", "samsung/hltedx/hlte:5.0/LRX21V/N9005DXUGBOK1:user/morningstar");
+            property_set("ro.build.description", "hltedx-user 5.0 LRX21V N9005DXUGBOK1 morningstar");
+            property_set("ro.product.name", "hltedx");
+        } else if (strstr(bootloader, "N9005VJ")) {
+            /* hlte/vj */
+            property_set("ro.build.fingerprint", "samsung/hltevj/hlte:5.0/LRX21V/N9005VJUGBOI3:user/morningstar");
+            property_set("ro.build.description", "hltevj-user 5.0 LRX21V N9005VJUGBOI3 morningstar");
+            property_set("ro.product.name", "hltevj");
+        } else if (strstr(bootloader, "N9005ZS")) {
+            /* hlte/zh */
+            property_set("ro.build.fingerprint", "samsung/hltezh/hlte:5.0/LRX21V/N9005ZSUGOI1:user/morningstar");
+            property_set("ro.build.description", "hltezh-user 5.0 LRX21V N9005ZSUGOI1 morningstar");
+            property_set("ro.product.name", "hltezh");
+        } else {
+            /* all other variants become hlte/xx */
+            property_set("ro.build.fingerprint", "samsung/hltexx/hlte:5.0/LRX21V/N9005XXUGBOK6:user/morningstar");
+            property_set("ro.build.description", "hltexx-user 5.0 LRX21V N9005XXUGBOK6 morningstar");
+            property_set("ro.product.name", "hltexx");
+        }
         property_set("ro.product.model", "SM-N9005");
-        property_set("ro.product.device", "hltexx");
+        property_set("ro.product.device", "hlte");
     }
 
     property_get("ro.product.device", device);
